9d31619a6c9de38933baaa770ec741b7e271d706
Luciano Martín Gamberale <lgamberale@fi.uba.ar>
2025-12-02 05:13:45 +0000

    :construction: first steps on controllers heartbeat

--- ToDo.md
+++ ToDo.md
@@ -1,12 +1,13 @@
 ToDo list (backlog):
-- hacer que el server guarde las sesiones que va teniendo
-- en cuanto se cae el server tiene que mandar un mensaje a todos para que limpien la información de todos los clientes (lo mismo pasa cuando se cae un cliente)
 - que el controller tenga dos procesos, uno escuchando el heartbeat y el otro haciendo lo de siempre, ambos controlados (lo mismo para el server)
+- cada controller tiene que limpiar todo lo referido a la session y pasar el mensaje a todos los siguientes
 - actualizar el rabbit para que persista siempre los mensajes
 
+- use _log functions on controllers
 - deberiamos imprimir lo que se va cargando de la metadata, total es una unica vez que se hace
 - implementar la espera del joiner de una mejor manera (capaz mandando un mensaje especial, porque potencialmente podría quedar en un loop infinito o quedar basura)
 - las sections del archivo que usan json son todas iguales, se pueden unificar en una jerarquia. 
+- ver que se tiro un sigterm de la nada en el cliente o server, puede explotar porque cierro el socket, y termina con un exit(1)
 - podemos dividir cuanto se le manda a cada lado de los controllers en los que se bifurca
 - aplicar patrón visitor
 - limpiar los @TODO
--- src/controllers/shared/controller.py
+++ src/controllers/shared/controller.py
@@ -1,8 +1,11 @@
 import logging
+import multiprocessing
 import signal
 import threading
 from abc import ABC, abstractmethod
-from typing import Any, Callable
+from typing import Any, Callable, Optional
+
+from shared.heartbeat_process import HeartbeatProcess
 
 
 class Controller(ABC):
@@ -41,6 +44,20 @@ class Controller(ABC):
         self._init_mom_consumers(rabbitmq_host, consumers_config)
         self._init_mom_producers(rabbitmq_host, producers_config)
 
+        self._heartbeat_process: Optional[multiprocessing.Process] = None
+        self._heartbeat_process_port = 555  # @TODO read from config
+
+    # ============================== PRIVATE - LOGGING ============================== #
+
+    def _log_debug(self, text: str) -> None:
+        logging.debug(f"{text} | pid: main_process")
+
+    def _log_info(self, text: str) -> None:
+        logging.info(f"{text} | pid: main_process")
+
+    def _log_error(self, text: str) -> None:
+        logging.error(f"{text} | pid: main_process")
+
     # ============================== PRIVATE - ACCESSING ============================== #
 
     def _is_running(self) -> bool:
@@ -52,25 +69,118 @@ class Controller(ABC):
     def _set_controller_as_running(self) -> None:
         self.is_stopped.clear()
 
+    # ============================== PRIVATE - HANDLE PROCESSES ============================== #
+
+    def _terminate_all_processes(self) -> None:
+        if self._heartbeat_process:
+            process = self._heartbeat_process
+            if process.is_alive():
+                process.terminate()
+                self._log_debug(
+                    f"action: terminate_process | result: success | pid: {process.pid}"
+                )
+
+        self._log_info("action: all_processes_terminate | result: success")
+
+    def _join_all_processes(self) -> None:
+        if self._heartbeat_process:
+            process = self._heartbeat_process
+            process.join()
+            pid = process.pid
+            exitcode = process.exitcode
+            self._log_info(
+                f"action: join_process | result: success | pid: {pid} | exitcode: {exitcode}"
+            )
+
+    def _close_all_processes(self) -> list[tuple[int, int]]:
+        uncaught_exceptions = []
+        if self._heartbeat_process:
+            process = self._heartbeat_process
+            pid = process.pid
+            exitcode = process.exitcode
+            if exitcode != 0:
+                uncaught_exceptions.append((pid, exitcode))
+
+            process.close()
+            self._log_debug(f"action: close_process | result: success | pid: {pid}")
+        return uncaught_exceptions  # type: ignore
+
+    def _join_non_alive_processes(self) -> None:
+        if self._heartbeat_process:
+            process = self._heartbeat_process
+            if not process.is_alive():
+                process.join()
+                pid = process.pid
+                exitcode = process.exitcode
+                self._log_info(
+                    f"action: join_process | result: success | pid: {pid} | exitcode: {exitcode}"
+                )
+
+                if exitcode != 0:
+                    # self._clean_up_zombie_session(session_id)
+                    self._log_error(
+                        f"action: process_error | result: error | pid: {pid} | exitcode: {exitcode}"
+                    )
+                else:
+                    process.close()
+                    self._heartbeat_process = None
+                    self._log_info(
+                        f"action: close_process | result: success | pid: {pid}"
+                    )
+
     # ============================== PRIVATE - SIGNAL HANDLER ============================== #
+
+    def _sigchld_signal_handler(self, signum: Any, frame: Any) -> None:
+        self._log_info("action: sigchld_signal_handler | result: in_progress")
+
+        self._join_non_alive_processes()
+
+        self._log_info("action: sigchld_signal_handler | result: success")
 
     @abstractmethod
     def _stop(self) -> None:
         raise NotImplementedError("subclass resposability")
 
     def _sigterm_signal_handler(self, signum: Any, frame: Any) -> None:
-        logging.info("action: sigterm_signal_handler | result: in_progress")
+        self._log_info("action: sigterm_signal_handler | result: in_progress")
 
         self._set_controller_as_stopped()
         self._stop()
 
-        logging.info("action: sigterm_signal_handler | result: success")
+        self._log_info("action: sigterm_signal_handler | result: success")
+
+    # ============================== PRIVATE - HEARTBEAT ============================== #
+
+    def _handle_heartbeat(self) -> None:
+        HeartbeatProcess(self._heartbeat_process_port).run()
+
+    def _handle_heartbeat_spawning_process(self) -> None:
+        process = multiprocessing.Process(target=self._handle_heartbeat, args=())
+        process.start()
+        self._heartbeat_process = process
+        self._log_info(f"action: spawn_heartbeat_process | result: success")
 
     # ============================== PRIVATE - RUN ============================== #
 
     def _run(self) -> None:
         self._set_controller_as_running()
-        logging.info("action: controller_running | result: success")
+        self._log_info("action: controller_running | result: success")
+        self._handle_heartbeat_spawning_process()
+
+    def _close_heartbeat_processes(self) -> None:
+        self._terminate_all_processes()
+        self._join_all_processes()
+        uncaught_exceptions = self._close_all_processes()
+        if len(uncaught_exceptions) != 0:
+            uncaught_exceptions_as_str = ", ".join(
+                [
+                    f"(pid: {pid}, exitcode: {exitcode})"
+                    for pid, exitcode in uncaught_exceptions
+                ]
+            )
+            raise Exception(
+                f"Some processes exited with errors: {uncaught_exceptions_as_str}"
+            )
 
     @abstractmethod
     def _close_all(self) -> None:
@@ -80,17 +190,18 @@ class Controller(ABC):
         try:
             callback()
         except Exception as e:
-            logging.error(f"action: controller_run | result: fail | error: {e}")
+            self._log_error(f"action: controller_run | result: fail | error: {e}")
             raise e
         finally:
             self._close_all()
-            logging.info("action: close_all | result: success")
+            self._close_heartbeat_processes()
+            self._log_info("action: close_all | result: success")
 
     # ============================== PUBLIC ============================== #
 
     def run(self) -> None:
-        logging.info("action: controller_startup | result: success")
+        self._log_info("action: controller_startup | result: success")
 
         self._ensure_connections_close_after_doing(self._run)
 
-        logging.info("action: controller_shutdown | result: success")
+        self._log_info("action: controller_shutdown | result: success")
